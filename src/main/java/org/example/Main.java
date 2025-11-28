b32da696c57a9eff02c013379a9872a25a0413aa
Manuel <mantel1337@gmail.com>
2025-11-28 14:30:38 +0100

    integrate into main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,18 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        System.out.println("Tennis");
+        Set set = new Set(Player.ONE);
+        do {
+            Scanner input = new Scanner(System.in);
+            int i = input.nextInt();
+            set.addPoint(i == 1 ? Player.ONE : Player.TWO);
+            String score = set.printScore();
+            System.out.println(score);
+        } while (true);
     }
 }
