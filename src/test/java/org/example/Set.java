def313e9921c2e316451d6a7419420c6799569bd
Manuel <mantel1337@gmail.com>
2025-11-28 13:28:36 +0100

    add server to output, fake rest

--- src/test/java/org/example/Set.java
+++ src/test/java/org/example/Set.java
@@ -1,13 +1,16 @@
 package org.example;
 
 public class Set {
+    private final Player server;
+
     public Set(Player server) {
+        this.server = server;
     }
 
     public void addPoint(Player player) {
     }
 
     public String printScore() {
-        return null;
+        return "Server: %s - %s - %s".formatted(server.name(), "Thirty", "Fifteen");
     }
 }
